409c9538f8850c6d3789352a279154b87ebcc2ae
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-15 19:16:01 +0300

    Initial Commit

---
+++ SwiftUI-WordScramble/SwiftUI_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WordScrambleApp.swift
+//  SwiftUI-WordScramble
+//
+//  Created by Nikita Tatarnikov on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
